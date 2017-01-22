cc12ff64323acc11b52c233b4bcdcc48794b5476
Aceywr <714044126@qq.com>
2017-01-22 23:23:13 +0800

    sync&async

---
+++ node.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Aceywr 
+ * @Date: 2017-01-22 18:28:25 
+ * @Last Modified by: Aceywr
+ * @Last Modified time: 2017-01-22 23:15:46
+ */
+/*
+var http = require('http');
+
+http.createServer(function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
+*/
